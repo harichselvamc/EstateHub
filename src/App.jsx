3fd79860501d3edaa78a9c67e4925953ad4f2227
harichselvam <harichselvamc@gmail.com>
2024-07-18 01:25:50 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import './layout.scss'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     hello world
-    </>
+  
+  <div className="layout">
+    hello
+  </div>
   )
 }
 
