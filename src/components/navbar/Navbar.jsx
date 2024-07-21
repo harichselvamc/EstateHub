2eb23b8cdf2dabe94f5be2a7ec19b5ddc2ff6e4d
harichselvam <harichselvamc@gmail.com>
2024-07-22 01:57:15 +0530

    ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
         {user ? (
           <div className="user">
             <img
-              src="src\assets\user.jpeg"
+              src="./user.jpeg"
               alt=""
             />
             <span>harichselvam</span>
